8f464e9a8c3ecfe59092485c8adc1d5de036383e
MartinSJosefsson <martin.s.josefsson@gmail.com>
2025-05-06 13:52:48 +0200

    Updated Main

--- src/main/java/se/lexicon/exceptions/workshop/Main.java
+++ src/main/java/se/lexicon/exceptions/workshop/Main.java
@@ -3,20 +3,33 @@ package se.lexicon.exceptions.workshop;
 import se.lexicon.exceptions.workshop.exception.DuplicateNameException;
 import se.lexicon.exceptions.workshop.data_access.NameService;
 import se.lexicon.exceptions.workshop.domain.Person;
+import se.lexicon.exceptions.workshop.exception.MyExceptionHandler;
 import se.lexicon.exceptions.workshop.fileIO.CSVReader_Writer;
 
 import java.io.IOException;
 import java.util.List;
 
 public class Main {
+
     public static void main(String[] args) {
         try {
+
             List<String> maleFirstNames = CSVReader_Writer.getMaleFirstNames();
             List<String> femaleFirstNames = CSVReader_Writer.getFemaleFirstNames();
+
             List<String> lastNames = CSVReader_Writer.getLastNames();
+
             NameService nameService = new NameService(maleFirstNames, femaleFirstNames, lastNames);
-            nameService.addFemaleFirstName("Alice");
+
+//            nameService.addFemaleFirstName("Alice");
+            nameService.addLastName("Andersson");
+
             Person test = nameService.getNewRandomPerson();
             System.out.println(test);
-        } catch (
-                Exception e) {            // pass the exception class to the handler class for handling and returning a proper message        }    }}
+        } catch (Exception e) {
+            MyExceptionHandler.handleExceptions(e);
+        }
+
+    }
+
+}
